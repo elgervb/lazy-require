88c63ed863527f2390d53f192d3c48df6730089a
Elger van Boxtel <elgervb@gmail.com>
2016-01-21 16:47:45 +0100

    sync loading of required module

--- index.js
+++ index.js
@@ -1,41 +1,69 @@
-/* global __dirname */
 'use strict';
-module.exports = (function(options) {
+
+module.exports = (function lazyRequire(options) {
   var npm = require('npm');
   var log = require('npmlog');
+  var deasync = require('deasync');
   
-  var packageJson = require(__dirname + '/package.json');
+  var basePath = __dirname + '/../..';
+  
+  var loadPackageJson = function() {
+    var fs = require('fs');
+    try {
+      fs.statSync(basePath + '/package.json');
+      log.info('Use package.json from ' + basePath + '/package.json');
+      return require(basePath + '/package.json');
+    } catch(err) {
+      try {
+        basePath = __dirname + '/.';
+        log.info('Use package.json from ' + basePath + '/package.json');
+        return require(basePath + '/package.json');
+      } catch (err2) {
+        // no package.json
+      }
+    }
+  };
+  var packageJson = loadPackageJson();
   
   var loadDev = function(module) {
     return requireNpm(module, true);
   };
+  
   var load = function(module) {
      return requireNpm(module, false);
   };
   var requireNpm = function(module, isDev) {
     
+    var result;
     var moduleString = loadModuleStringFromPackageJson(module, isDev);
     
-    return (function(moduleString, moduleName) {
-      
-      return npm.load({}, function error(err){
+    (function syncLoad(moduleString, moduleName) {
+      npm.load({}, function error(err){
         if (err) throw err;
         
-        var modulePath = __dirname + '/node_modules/' + moduleName
+        var modulePath = basePath + '/node_modules/' + moduleName
         try {
-            return require(modulePath);
+            result = require(modulePath);
         } catch (loadError) {
           if (loadError.code && loadError.code === 'MODULE_NOT_FOUND') {
             log.info('Loading ' + moduleString +'...');
             npm.commands.install([moduleString], function (er, data) {
               if (er) throw er
               log.info('Installed module ' + module + ' with ' + data.length + ' dependencies');
-              return require(modulePath);
+              result = require(modulePath);
             })
-          };
+          } else {
+            throw loadError;
+          }
         }
       });
     })(moduleString, module);
+    
+    while ( result === undefined) {
+      deasync.sleep(100);
+    }
+    
+    return result;
   };
   
   var fixVersion = function(version) {
@@ -57,7 +85,7 @@ module.exports = (function(options) {
       return module;
     }
     
-    log.info('Loading ' + module + ' module which is not mentioned in package.json');
+    log.info('Loading ' + module + ' module which is not mentioned in package.json ' + packageJson.name);
     return module;
   }
   
