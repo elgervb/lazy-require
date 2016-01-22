3ed07339326e29d8ed4591a5e302f78ff91f1ef7
Elger van Boxtel <elgervb@gmail.com>
2016-01-22 11:19:28 +0100

    shorten require path

--- test.js
+++ test.js
@@ -1,7 +1,10 @@
 /* global __dirname */
-var lazyRequire = require(__dirname + '/index.js');
+var lazyRequire = require(__dirname);
+lazyRequire.options({basepath:'./'});
+
 
 var mod = lazyRequire.load('yargs');
 
 console.assert(mod, 'Module not loaded');
 
+console.log('done!');
