07c6adda1e52c46b8645ff8914dab98be7c8ba3b
Elger van Boxtel <elgervb@gmail.com>
2016-01-22 11:45:14 +0100

    delete module directory before beginning test

--- test.js
+++ test.js
@@ -3,6 +3,26 @@ var lazyRequire = require(__dirname);
 lazyRequire.options({basepath:'./'});
 
 
+function rmdirSync(dir){
+  var fs = require('fs');
+  var s = fs.lstatSync(dir);
+  if(s.isFile()){
+    fs.unlinkSync(dir);
+    return;
+  }
+  if(!s.isDirectory()){
+    return;
+  }
+
+  var fileArr = fs.readdirSync(dir);
+  for(var f in fileArr){
+    rmdirSync(dir+'/'+fileArr[f]);
+  }
+  console.log('deleting dir ' + dir);
+  fs.rmdirSync(dir);
+}
+
+rmdirSync('./node_modules/yargs');
 var mod = lazyRequire.load('yargs');
 
 console.assert(mod, 'Module not loaded');
